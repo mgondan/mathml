207a74a100671fcb748d741e963897735fad79ab
ialfarone <irene.alfarone@gmail.com>
2022-12-12 16:33:19 +0100

    small changes

--- inst/pl/mathml.pl
+++ inst/pl/mathml.pl
@@ -2227,6 +2227,11 @@ math(Flags, omit(Expr), New, M),
     option(error(ignore), Flags, highlight)
  => Flags = New,
     M = Expr.
+    
+math(Flags, omit(_Expr), New, M),
+    option(error(asis), Flags, highlight)
+ => Flags = New,
+    M = "". % suppress at the next level, in the list
 
 math(Flags, omit(Expr), New, M),
     option(error(fix), Flags, highlight)
