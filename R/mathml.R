8e85b6730b7aad2f7830988f1653dd2491269bf1
mgondan <Matthias.Gondan@psy.ku.dk>
2023-07-06 14:57:43 +0200

    Improve rolog tests

--- R/mathml.R
+++ R/mathml.R
@@ -5,8 +5,11 @@
 
 .onAttach <- function(libname, pkgname)
 {
-  if(!requireNamespace("rolog", quietly=TRUE))
-    stop("Could not attach library rolog.")
+  if(!requireNamespace("rolog", quiet=TRUE))
+    stop("Could not load R package rolog.")
+
+  if(!rolog::rolog_ok())
+    stop("Could not attach R package rolog.")
 
   rolog::consult(system.file("pl/mathml.pl", package=pkgname))
 }
