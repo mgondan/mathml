1735bb6f3cb50e13ec8279ea86bcc988f0520d6b
jeremyirilli <jeremy.irilli@student.uibk.ac.at>
2024-03-23 18:10:29 +0100

    Replaced 'frac' with table representation in 'rcond'

--- inst/pl/bussproofs.pl
+++ inst/pl/bussproofs.pl
@@ -17,10 +17,28 @@
 
 % Render in MathML
 mlx(rcond(A, B), M, Flags) :-
+    build_table([B], [##(A)], M1),
+    M2 = [##('') | M1],
+    M3 =.. [### | M2],
+    ml(proof_tree(M3), M4, Flags),
+    M = mrow([M4]).
+ 
+build_table([], Acc, Acc).
+
+build_table([B], Acc, Matrix) :-
+    B =.. L,
+    nth0(0, L, Func, L1),
+    nth0(0, L1, Arg, L2),
+    B1 =.. [Func, Arg],
+    append([##(B1)], Acc, List),
+    build_table(L2, List, Matrix). 
+
+/* 
+mlx(rcond(A, B), M, Flags) :-
     ml(frac(B, A), F1, Flags),
     ml('%->%'('R', ''), R1, Flags),
     M = mrow([F1, mstyle([mathsize('0.7em')], R1)]).
- 
+
 mlx(land(A, B), M, Flags) :-
     ml(frac(B, A), F1, Flags),
     ml('&'('L', ''), R1, Flags),
@@ -29,9 +47,7 @@ mlx(land(A, B), M, Flags) :-
 mlx(ax(A, B), M, Flags) :-
     ml(frac(B, A), F1, Flags),
     ml(ident('Ax.'), R1, [mathvariant(italic)]),
-    M = mrow([F1, mstyle([mathsize('0.7em')], R1)]).
-
-
+    M = mrow([F1, mstyle([mathsize('0.7em')], R1)]). */
 
 % Render in MathJax
 % jaxx(rcond(A, B), M, Flags) :-
