9c78f92fde8ef6ce4e8dad51fce61f7279367217
jeremyirilli <jeremy.irilli@student.uibk.ac.at>
2024-04-13 09:38:24 +0200

    Moving 'pval' to separate file

--- inst/pl/mathml.pl
+++ inst/pl/mathml.pl
@@ -13,6 +13,8 @@
 %
 :- dynamic math_hook/2.
 :- multifile math_hook/2.
+:- multifile math/4.
+:- multifile math/3.
 
 % Low-level functions (see, e.g. nthroot.pl)
 %
@@ -1778,34 +1780,6 @@ math(number(A), M),
 
 math(number(A), M)
  => M = pos(A).
-
-% p-value
-math(pval(A), M, Flags, Flags1),
-    type(A, T, Flags),
-    member(numeric(N), T),
-    N =< 1,
-    N >= 0.1
- => M = A,
-    Flags1 = [round(2) | Flags].
-
-math(pval(A), M, Flags, Flags1),
-    type(A, T, Flags),
-    member(numeric(_N), T)
- => M = A,
-    Flags1 = [round(3) | Flags].
-
-math(pval(A), M, Flags, Flags1)
- => M = A,
-    Flags1 = Flags.
-
-math(pval(A, P), M, Flags),
-    type(A, T, Flags),
-    member(numeric(N), T),
-    N < 0.001
- => M = (P < pval(0.001)).
-
-math(pval(A, P), M, _Flags)
- => M = (P == pval(A)).
 
 % Operators
 math(isin(A, B), X)
