5bb71931eec5b2e63a890657365e07584ae8ef1b
Gondan-Rochon <c7201178@uibk.ac.at>
2024-04-15 12:15:59 +0200

    Add pval, math_hook/3,4

--- inst/pl/pval.pl
+++ inst/pl/pval.pl
@@ -1,57 +1,32 @@
 % p-value
-math(pval(A), M, Flags, Flags1),
-    type(A, T, Flags),
-    member(numeric(N), T),
-    N =< 1,
-    N >= 0.1
- => M = A,
-    Flags1 = [round(2) | Flags].
-
-math(pval(A), M, Flags, Flags1),
-    type(A, T, Flags),
-    member(numeric(_N), T)
- => M = A,
-    Flags1 = [round(3) | Flags].
-
-math(pval(A), M, Flags, Flags1)
- => M = A,
-    Flags1 = Flags.
-
-math(pval(A, P), M, Flags),
-    type(A, T, Flags),
-    member(numeric(N), T),
-    N < 0.001
- => M = (P < pval(0.001)).
-
-math(pval(A, P), M, _Flags)
- => M = (P == pval(A)).
-
-
-/* 
-% p-value
-math(pval(A), M, Flags, Flags1) :-
+math_hook(pval(A), M, Flags, Flags1) :-
     type(A, T, Flags),
     member(numeric(N), T),
     N =< 1,
     N >= 0.1,
+    !,
     M = A,
     Flags1 = [round(2) | Flags].
 
-math(pval(A), M, Flags, Flags1) :-
+math_hook(pval(A), M, Flags, Flags1) :-
     type(A, T, Flags),
     member(numeric(_N), T),
+    !,
     M = A,
     Flags1 = [round(3) | Flags].
 
-math(pval(A), M, Flags, Flags1) :- 
+math_hook(pval(A), M, Flags, Flags1) :-
+    !,
     M = A,
     Flags1 = Flags.
 
-math(pval(A, P), M, Flags) :-
+math_hook(pval(A, P), M, Flags) :-
     type(A, T, Flags),
     member(numeric(N), T),
     N < 0.001,
+    !,
     M = (P < pval(0.001)).
 
-math(pval(A, P), M, _Flags) :-
-    M = (P == pval(A)). */
+math_hook(pval(A, P), M, _Flags) :-
+    !,
+    M = (P == pval(A)).
