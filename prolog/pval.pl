e3f2db96963bbd5a265fe044f09d49f394d992c6
Gondan-Rochon <c7201178@uibk.ac.at>
2024-04-15 12:24:47 +0200

    pval/1,2 in prolog module

--- prolog/pval.pl
+++ prolog/pval.pl
@@ -1,5 +1,10 @@
+:- module(pval, []).
+:- reexport(library(mathml)).
+
+:- multifile math_hook/2, math_hook/3, math_hook/4.
+
 % p-value
-math_hook(pval(A), M, Flags, Flags1) :-
+mathml:math_hook(pval(A), M, Flags, Flags1) :-
     type(A, T, Flags),
     member(numeric(N), T),
     N =< 1,
@@ -8,25 +13,25 @@ math_hook(pval(A), M, Flags, Flags1) :-
     M = A,
     Flags1 = [round(2) | Flags].
 
-math_hook(pval(A), M, Flags, Flags1) :-
+mathml:math_hook(pval(A), M, Flags, Flags1) :-
     type(A, T, Flags),
     member(numeric(_N), T),
     !,
     M = A,
     Flags1 = [round(3) | Flags].
 
-math_hook(pval(A), M, Flags, Flags1) :-
+mathml:math_hook(pval(A), M, Flags, Flags1) :-
     !,
     M = A,
     Flags1 = Flags.
 
-math_hook(pval(A, P), M, Flags) :-
+mathml:math_hook(pval(A, P), M, Flags) :-
     type(A, T, Flags),
     member(numeric(N), T),
     N < 0.001,
     !,
     M = (P < pval(0.001)).
 
-math_hook(pval(A, P), M, _Flags) :-
+mathml:math_hook(pval(A, P), M, _Flags) :-
     !,
     M = (P == pval(A)).
