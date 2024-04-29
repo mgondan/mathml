3d7d65e6dd76f8c149740a902097dc734540d348
mgondan <Matthias.Gondan-Rochon@uibk.ac.at>
2024-04-29 12:28:31 +0200

    Update mathml.pl

--- prolog/mathml.pl
+++ prolog/mathml.pl
@@ -1796,34 +1796,6 @@ math(number(A), M),
 math(number(A), M)
  => M = pos(A).
 
-% p-value
-math(pval(A), M, Flags, Flags1),
-    type(A, T, Flags),
-    member(numeric(N), T),
-    N =< 1,
-    N >= 0.1
- => M = A,
-    Flags1 = [round(2) | Flags].
-
-math(pval(A), M, Flags, Flags1),
-    type(A, T, Flags),
-    member(numeric(_N), T)
- => M = A,
-    Flags1 = [round(3) | Flags].
-
-math(pval(A), M, Flags, Flags1)
- => M = A,
-    Flags1 = Flags.
-
-math(pval(A, P), M, Flags),
-    type(A, T, Flags),
-    member(numeric(N), T),
-    N < 0.001
- => M = (P < pval(0.001)).
-
-math(pval(A, P), M, _Flags)
- => M = (P == pval(A)).
-
 % Operators
 math(isin(A, B), X)
  => current_op(Prec, xfx, =),
