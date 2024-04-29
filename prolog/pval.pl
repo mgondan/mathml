2a52ba99c1427adb7502d371b0d05f349b0a5f76
mgondan <Matthias.Gondan-Rochon@uibk.ac.at>
2024-04-29 12:29:17 +0200

    Update pval.pl

--- prolog/pval.pl
+++ prolog/pval.pl
@@ -4,7 +4,7 @@
 :- multifile math_hook/2, math_hook/3, math_hook/4.
 
 % p-value
-math_hook(pval(A), M, Flags, Flags1) :-
+mathml:math_hook(pval(A), M, Flags, Flags1) :-
     mathml:type(A, T, Flags),
     member(numeric(N), T),
     N =< 1,
@@ -13,25 +13,25 @@ math_hook(pval(A), M, Flags, Flags1) :-
     M = A,
     Flags1 = [round(2) | Flags].
 
-math_hook(pval(A), M, Flags, Flags1) :-
+mathml:math_hook(pval(A), M, Flags, Flags1) :-
     mathml:type(A, T, Flags),
     member(numeric(_N), T),
     !,
     M = A,
     Flags1 = [round(3) | Flags].
 
-math_hook(pval(A), M, Flags, Flags1) :-
+mathml:math_hook(pval(A), M, Flags, Flags1) :-
     !,
     M = A,
     Flags1 = Flags.
 
-math_hook(pval(A, P), M, Flags) :-
+mathml:math_hook(pval(A, P), M, Flags) :-
     mathml:type(A, T, Flags),
     member(numeric(N), T),
     N < 0.001,
     !,
     M = (P < pval(0.001)).
 
-math_hook(pval(A, P), M, _Flags) :-
+mathml:math_hook(pval(A, P), M, _Flags) :-
     !,
     M = (P == pval(A)).
