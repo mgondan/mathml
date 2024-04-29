e86d101baeea471dace6a8b7a02492e22198a104
mgondan <Matthias.Gondan-Rochon@uibk.ac.at>
2024-04-29 12:37:55 +0200

    Update bussproofs.pl

--- prolog/bussproofs.pl
+++ prolog/bussproofs.pl
@@ -24,18 +24,18 @@
 mathml:mlx(proof(Denominator, Numerator), M, Flags) :-
     X1 =.. ['###1', '##'(Numerator)],
     X =.. ['###2', '##'(X1), '##1'(Denominator)],
-    ml(proof_tree(X), M, Flags).
+    mathml:ml(proof_tree(X), M, Flags).
 
 mathml:mlx(proof(Denominator, Numerator1, Numerator2), M, Flags) :-
     X1 =.. ['###1', '##'(Numerator1, '', Numerator2)],
     X =.. ['###2', '##'(X1), '##1'(Denominator)],
-    ml(proof_tree(X), M, Flags).
+    mathml:ml(proof_tree(X), M, Flags).
 
 mathml:mlx(format_operator(Op), R, Flags) :-
-    ml(Op, R1, Flags),
-    ml(size(R1, '0.7em'), R2, Flags),
-    ml(mstyle_right(R2), R3, Flags),
-    ml(mpadded_right(R3), R, Flags).
+    mathml:ml(Op, R1, Flags),
+    mathml:ml(size(R1, '0.7em'), R2, Flags),
+    mathml:ml(mstyle_right(R2), R3, Flags),
+    mathml:ml(mpadded_right(R3), R, Flags).
 
 mathml:mlx(size(A, Size), M, _Flags) :-
     M = mrow([mstyle([mathsize(Size)], A)]).
@@ -47,118 +47,111 @@ mathml:mlx(mpadded_right(A), M, _Flags) :-
     M = mpadded([height('.5em'), width('.5em'), voffset('.9em'), semantics('bspr_prooflabel:right')], A).
 
 mathml:mlx(rbicond(A, B), M, Flags) :-
-    ml(proof(A, B), F, Flags),
-    ml(format_operator('%<->%'('R', '')), R, Flags),
+    mathml:ml(proof(A, B), F, Flags),
+    mathml:ml(format_operator('%<->%'('R', '')), R, Flags),
     M = mrow([F, R]).  
 
 mathml:mlx(rbicond(A, B, C), M, Flags) :-
-    ml(proof(A, B, C), F, Flags),
-    ml(format_operator('%<->%'('R', '')), R, Flags),
+    mathml:ml(proof(A, B, C), F, Flags),
+    mathml:ml(format_operator('%<->%'('R', '')), R, Flags),
     M = mrow([F, R]).  
 
 mathml:mlx(rcond(A, B), M, Flags) :-
-    ml(proof(A, B), F, Flags),
-    ml(format_operator('%->%'('R', '')), R, Flags),
+    mathml:ml(proof(A, B), F, Flags),
+    mathml:ml(format_operator('%->%'('R', '')), R, Flags),
     M = mrow([F, R]).  
 
 mathml:mlx(rcond(A, B, C), M, Flags) :-
-    ml(proof(A, B, C), F, Flags),
-    ml(format_operator('%->%'('R', '')), R, Flags),
+    mathml:ml(proof(A, B, C), F, Flags),
+    mathml:ml(format_operator('%->%'('R', '')), R, Flags),
     M = mrow([F, R]).  
 
 mathml:mlx(rand(A, B), M, Flags) :-
-    ml(proof(A, B), F, Flags),
-    ml(format_operator(and('R', '')), R, Flags),
+    mathml:ml(proof(A, B), F, Flags),
+    mathml:ml(format_operator(and('R', '')), R, Flags),
     M = mrow([F, R]). 
 
 mathml:mlx(rand(A, B, C), M, Flags) :-
-    ml(proof(A, B, C), F, Flags),
-    ml(format_operator(and('R', '')), R, Flags),
+    mathml:ml(proof(A, B, C), F, Flags),
+    mathml:ml(format_operator(and('R', '')), R, Flags),
     M = mrow([F, R]). 
     
 mathml:mlx(ror(A, B), M, Flags) :-
-    ml(proof(A, B), F, Flags),
-    ml(format_operator(or('R', '')), R, Flags),
+    mathml:ml(proof(A, B), F, Flags),
+    mathml:ml(format_operator(or('R', '')), R, Flags),
     M = mrow([F, R]). 
 
 mathml:mlx(ror(A, B, C), M, Flags) :-
-    ml(proof(A, B, C), F, Flags),
-    ml(format_operator(or('R', '')), R, Flags),
+    mathml:ml(proof(A, B, C), F, Flags),
+    mathml:ml(format_operator(or('R', '')), R, Flags),
     M = mrow([F, R]). 
 
 mathml:mlx(rneg(A, B), M, Flags) :-
-    ml(proof(A, B), F, Flags),
-    ml(format_operator(!('R','')), R, Flags),
+    mathml:ml(proof(A, B), F, Flags),
+    mathml:ml(format_operator(!('R','')), R, Flags),
     M = mrow([F, R]). 
 
-mlx(rneg(A, B, C), M, Flags) :-
-    ml(proof(A, B, C), F, Flags),
-    ml(format_operator(!('R','')), R, Flags),
+mathml:mlx(rneg(A, B, C), M, Flags) :-
+    mathml:ml(proof(A, B, C), F, Flags),
+    mathml:ml(format_operator(!('R','')), R, Flags),
     M = mrow([F, R]). 
 
-mlx(lbicond(A, B), M, Flags) :-
-    ml(proof(A, B), F, Flags),
-    ml(format_operator('%<->%'('L', '')), R, Flags),
+mathml:mlx(lbicond(A, B), M, Flags) :-
+    mathml:ml(proof(A, B), F, Flags),
+    mathml:ml(format_operator('%<->%'('L', '')), R, Flags),
     M = mrow([F, R]). 
 
 mathml:mlx(lbicond(A, B, C), M, Flags) :-
-    ml(proof(A, B, C), F, Flags),
-    ml(format_operator('%<->%'('L', '')), R, Flags),
+    mathml:ml(proof(A, B, C), F, Flags),
+    mathml:ml(format_operator('%<->%'('L', '')), R, Flags),
     M = mrow([F, R]).  
 
 mathml:mlx(lcond(A, B), M, Flags) :-
-    ml(proof(A, B), F, Flags),
-    ml(format_operator('%->%'('L', '')), R, Flags),
+    mathml:ml(proof(A, B), F, Flags),
+    mathml:ml(format_operator('%->%'('L', '')), R, Flags),
     M = mrow([F, R]).  
 
 mathml:mlx(lcond(A, B, C), M, Flags) :-
-    ml(proof(A, B, C), F, Flags),
-    ml(format_operator('%->%'('L', '')), R, Flags),
+    mathml:ml(proof(A, B, C), F, Flags),
+    mathml:ml(format_operator('%->%'('L', '')), R, Flags),
     M = mrow([F, R]).  
 
 mathml:mlx(land(A, B), M, Flags) :-
-    ml(proof(A, B), F, Flags),
-    ml(format_operator('&'('L', '')), R, Flags),
+    mathml:ml(proof(A, B), F, Flags),
+    mathml:ml(format_operator('&'('L', '')), R, Flags),
     M = mrow([F, R]).  
 
 mathml:mlx(land(A, B, C), M, Flags) :-
-    ml(proof(A, B, C), F, Flags),
-    ml(format_operator('&'('L', '')), R, Flags),
+    mathml:ml(proof(A, B, C), F, Flags),
+    mathml:ml(format_operator('&'('L', '')), R, Flags),
     M = mrow([F, R]).  
 
 mathml:mlx(lor(A, B), M, Flags) :-
-    ml(proof(A, B), F, Flags),
-    ml(format_operator(or('L', '')), R, Flags),
+    mathml:ml(proof(A, B), F, Flags),
+    mathml:ml(format_operator(or('L', '')), R, Flags),
     M = mrow([F, R]). 
 
 mathml:mlx(lor(A, B, C), M, Flags) :-
-    ml(proof(A, B, C), F, Flags),
-    ml(format_operator(or('L', '')), R, Flags),
+    mathml:ml(proof(A, B, C), F, Flags),
+    mathml:ml(format_operator(or('L', '')), R, Flags),
     M = mrow([F, R]). 
 
 mathml:mlx(lneg(A, B), M, Flags) :-
-    ml(proof(A, B), F, Flags),
-    ml(format_operator(!('L', '')), R, Flags),
+    mathml:ml(proof(A, B), F, Flags),
+    mathml:ml(format_operator(!('L', '')), R, Flags),
     M = mrow([F, R]). 
 
 mathml:mlx(lneg(A, B, C), M, Flags) :-
-    ml(proof(A, B, C), F, Flags),
-    ml(format_operator(!('L', '')), R, Flags),
+    mathml:ml(proof(A, B, C), F, Flags),
+    mathml:ml(format_operator(!('L', '')), R, Flags),
     M = mrow([F, R]). 
 
 mathml:mlx(ax(A, B), M, Flags) :-
-    ml(proof(A, B), F, Flags),
-    ml(format_operator(ident('Ax.')), R, [mathvariant(italic)]),
+    mathml:ml(proof(A, B), F, Flags),
+    mathml:ml(format_operator(ident('Ax.')), R, [mathvariant(italic)]),
     M = mrow([F, R]).
 
 mathml:mlx(asq(A, B), M, Flags) :-
-    ml(proof(A, B), F, Flags),
-    ml(format_operator(ident('Asq.')), R, [mathvariant(italic)]),
+    mathml:ml(proof(A, B), F, Flags),
+    mathml:ml(format_operator(ident('Asq.')), R, [mathvariant(italic)]),
     M = mrow([mathcolor(red)], [F, R]).
-
-% Render in MathJax
-% jaxx(rcond(A, B), M, Flags) :-
-%     jax(A, A1, Flags),
-%     jax(B, B1, Flags),
-%     format(string(M), "\\frac{~w}{~w}", [B1, A1]).
-
