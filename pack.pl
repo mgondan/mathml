1f5716486009fcc0ed90183523b79364b14be7b5
mgondan <Matthias.Gondan-Rochon@uibk.ac.at>
2025-03-16 19:08:21 +0100

    add dependency rolog

--- pack.pl
+++ pack.pl
@@ -2,3 +2,4 @@ name(mathml).
 title('Translates mathematical expressions to compounds that can be used by html//1 (SWISH) or html//3 (normal SWIPL, with pack http/html_write).').
 version('1.3').
 author('Matthias Gondan', 'Matthias.Gondan-Rochon@uibk.ac.at').
+requires(rolog).
